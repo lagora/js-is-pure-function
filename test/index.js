f3890ac98d29ca2f38d64b1a906f7d16042e20d9
Franck Lagora <lagora@users.noreply.github.com>
2016-09-09 12:00:40 +0200

    WIP unit testing

---
+++ test/index.js
@@ -0,0 +1,16 @@
+var isPureFunction = require('../index.js');
+
+describe('isPureFunction', function () {
+  it('must return TRUE if a function take at least one argument', function () {
+    var f = function(a) { return a; };
+    assert(f.length > 0, isPureFunction(f));
+  });
+  it('must return FALSE if a function take no argument', function () {
+    var f = function() {};
+    assert(f.length ===0, isPureFunction(f));
+  });
+  it('must return TRUE if a function return something other than undefined', function () {
+    var f = function(a) { return a; };
+    assert(f(1), isPureFunction(f));
+  });
+});
